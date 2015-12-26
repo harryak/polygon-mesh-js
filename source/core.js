c5e03292c9f4da54b2f615743235c2e7f07b62e4
Felix Rossmann <felixrak@web.de>
2015-12-26 13:22:46 +0100

    added first sources

---
+++ source/core.js
@@ -0,0 +1,57 @@
+/**
+ * Defines the polygon mesh JS object.
+ * @author Felix Rossmann
+ */
+PMJS = {
+};
+
+/**
+ * @class Array
+ * @author Matthew Wagerfield
+ */
+PMJS.Array = typeof Float32Array === 'function' ? Float32Array : Array;
+
+/**
+ * @class Utils
+ * @author Matthew Wagerfield
+ */
+PMJS.Utils = {
+  isNumber: function(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
+  }
+};
+
+/**
+ * Request Animation Frame Polyfill.
+ * @author Paul Irish
+ * @see https://gist.github.com/paulirish/1579671
+ */
+(function() {
+
+  var lastTime = 0;
+  var vendors = ['ms', 'moz', 'webkit', 'o'];
+
+  for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+    window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+    window.cancelAnimationFrame  = window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+  }
+
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      var currentTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currentTime - lastTime));
+      var id = window.setTimeout(function() {
+        callback(currentTime + timeToCall);
+      }, timeToCall);
+      lastTime = currentTime + timeToCall;
+      return id;
+    };
+  }
+
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = function(id) {
+      clearTimeout(id);
+    };
+  }
+
+}());
---
+++ source/geometry.js
@@ -0,0 +1,18 @@
+/**
+ *	@class Geometry
+ *	@author Felix Rossmann
+ */
+PMJS.Geometry = function() {
+  this.vertices = [];
+  this.connections = [];
+  this.plsUpdate = false;
+};
+
+PMJS.Geometry.prototype = {
+  update: function() {
+    if (this.plsUpdate) {
+      this.plsUpdate = false;
+    }
+    return this;
+  }
+};
